9e8224ef1daa423f259c2e077304c89044d6ec85
Chelsea Li <148104000+Chelsealily@users.noreply.github.com>
2025-08-08 13:08:43 +0100

    updated icons

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
 import './App.scss'
 import ScrollToTop from 'react-scroll-to-top'
-import logoInsta from './assets/instagram.png'
-import logoSalary from "./assets/coin.png"
+import logoInsta from './assets/insta.svg'
+import logoSalary from "./assets/payp.svg"
+import logoPort from "./assets/pint.svg"
+import logoSpot from "./assets/spot.svg"
+import logoImdb from "./assets/imdb.svg"
 import Slideshow from "./Components/slideshow"
 import Headshots from './Components/headshots'
 import logo from './assets/chelsea li title logo.png'
@@ -25,20 +28,23 @@ function App() {
       <Headshots/>
 
       <span className="welcome-icons">
+
+        <a href="https://www.imdb.com/name/nm5891887/" target="_blank">
+        <img className="welcome__logo" width="60px" src={logoImdb} alt="Imdb logo"/></a>
+
+        <a href="https://app.spotlight.com/9118-3423-3884" target="_blank">
+        <img className="welcome__logo" width="60px" src={logoSpot} alt="Spotlight logo"/></a>
+
+      <a href="https://uk.pinterest.com/cbean0261/chelsea-li/?invite_code=f50a35a67c2c4d779cce7e3290f29c10&sender=1021332159153324013" target="_blank">
+        <img className="welcome__logo" width="60px" src={logoPort} alt="pinterest logo"/></a>
+
       <a href="https://www.instagram.com/chelsea.lily/" target="_blank">
         <img className="welcome__logo" width="60px" src={logoInsta} alt="instagram logo"/></a>
 
         <a href="https://www.paypal.com/paypalme/chelsealily" target="_blank">
         <img className="welcome__logo" width="60px" src={logoSalary} alt="paypal logo"/></a>
-        </span>
 
-      <div className="btn-group">
-      <a href="https://uk.pinterest.com/cbean0261/chelsea-li/?invite_code=f50a35a67c2c4d779cce7e3290f29c10&sender=1021332159153324013"><button>Portfolio</button></a>
-      <a href="https://www.imdb.com/name/nm5891887/"><button>Imdb</button></a>
-      <a href="https://app.spotlight.com/9118-3423-3884"><button>Spotlight</button></a>
-      <a href="https://www.sandrareynolds.co.uk/model/chelsea-li"><button>Sandra Reynolds</button></a>
-      <a href="https://www.roctalentagency.com/commercial-women/chelsea"><button>Roc Talent</button></a>
-      </div>
+        </span>
 
 
   
