5a047b1510e9ca03649f7a9d9aec64ec3155e327
Chelsea Li <148104000+Chelsealily@users.noreply.github.com>
2025-08-14 10:21:01 +0100

    fixed screen width

--- src/youtube.tsx
+++ src/youtube.tsx
@@ -1,24 +1,40 @@
 import "./youtube.scss";
 
 const Youtube = () => {
-
   return (
     <div className="youtube-container">
-        <h1>Showreels</h1>
-        <br></br>
-<iframe width="560" height="315" src="https://www.youtube.com/embed/gDp0W4p0MCo?si=Rk2qKMrxI4QKanGj" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" ></iframe>
+      <h1>Showreels</h1>
+      <br />
 
-      <br></br>
-<iframe width="560" height="315" src="https://www.youtube.com/embed/jGTMujWIoWc?si=oRbo1OJbbBE1aNwB" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" ></iframe>
-           <br></br>
-           <iframe width="560" height="315" src="https://www.youtube.com/embed/EYoqhfPNzVo?si=HSaWthGL_-Lw41gl" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
-           
-            </div>
+      <div className="video-wrapper">
+        <iframe
+          src="https://www.youtube.com/embed/gDp0W4p0MCo?si=Rk2qKMrxI4QKanGj"
+          title="YouTube video player"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        ></iframe>
+      </div>
 
+      <br />
+
+      <div className="video-wrapper">
+        <iframe
+          src="https://www.youtube.com/embed/jGTMujWIoWc?si=oRbo1OJbbBE1aNwB"
+          title="YouTube video player"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        ></iframe>
+      </div>
+
+      <br />
+
+      <div className="video-wrapper">
+        <iframe
+          src="https://www.youtube.com/embed/EYoqhfPNzVo?si=HSaWthGL_-Lw41gl"
+          title="YouTube video player"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        ></iframe>
+      </div>
+    </div>
   );
 };
 
 export default Youtube;
-
-
-
